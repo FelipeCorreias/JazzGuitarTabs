ddd5634fd2f16bc22b7082078273634fdc48c3bd
Felipe Correia <felipe@fcorreia.com.br>
2018-05-30 17:19:28 -0300

    .

--- JazzGuitarTabs.Domain/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace JazzGuitarTabs.Domain
-{
-    public class Class1
-    {
-    }
-}
---
+++ JazzGuitarTabs.Domain/Tabs/Tab.cs
@@ -0,0 +1,19 @@
+﻿using JazzGuitarTabs.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JazzGuitarTabs.Domain.Tabs
+{
+    public class Tab : Entity
+    {
+        public string Title { get; set; }
+        public string Artist { get; set; }
+        public string Style { get; set; }
+        public string FileName { get; set; }
+        public byte[] File { get; set; }
+        public string Tags { get; set; }
+        public bool IsApproved { get; set; } = false;
+        public string Author { get; set; }
+    }
+}
